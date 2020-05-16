a7649bcedc52b7a2da490ccd8c2fb8b1649887b2
Prashant kumar <prashant.kumar@technia.com>
2020-05-16 21:34:18 +0530

    refactored code to remove span and dot net code

--- login.js
+++ login.js
@@ -1,18 +1,18 @@
-// var attempt = 3;
-// function validate() {
-//     console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxx");
-//     var username = document.getElementById("adminUserId").value;
-//     var password = document.getElementById("adminPassword").value;
-//     if (username == "admin" && password == "admin") {
-//         return true;
-//     } else {
-//         attempt--;
-//         alert("You have left " + attempt + " attempt;");
-//         if (attempt == 0) {
-//             document.getElementById("adminUserId").disabled = true;
-//             document.getElementById("adminPassword").disabled = true;
-//             document.getElementById("login").disabled = true;
-//         }
-//         return false;
-//     }
-// }
+var attempt = 3;
+function validate() {
+    console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxx");
+    var username = document.getElementById("adminUserId").value;
+    var password = document.getElementById("adminPassword").value;
+    if (username == "admin" && password == "admin") {
+        return true;
+    } else {
+        attempt--;
+        alert("You have left " + attempt + " attempt;");
+        if (attempt == 0) {
+            document.getElementById("adminUserId").disabled = true;
+            document.getElementById("adminPassword").disabled = true;
+            document.getElementById("login").disabled = true;
+        }
+        return false;
+    }
+}
