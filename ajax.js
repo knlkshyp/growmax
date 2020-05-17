985daad832115b2d7fc61988e3140280f3e96d5b
Kunal Kashyap <kunal.kashyap@technia.com>
2020-05-17 20:06:11 +0530

    Handled error for absent divs

--- ajax.js
+++ ajax.js
@@ -1,13 +1,15 @@
 async function getData(url, field, id) {
     const element = document.getElementById(id);
-    const response = await fetch(`/${url}`, {
-        method: 'GET'
-      });
-    const json = await response.json();
-    for (index = 0; index < json.length; index++) {
-        const option = document.createElement('OPTION');
-        option.text = json[index][`${field}`];
-        element.add(option);
+    if (element !== null) {
+        const response = await fetch(`/${url}`, {
+            method: 'GET'
+        });
+        const json = await response.json();
+        for (index = 0; index < json.length; index++) {
+            const option = document.createElement('OPTION');
+            option.text = json[index][`${field}`];
+            element.add(option);
+        }
     }
 }
 
